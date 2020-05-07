a7beb63fff898bc9a09029d512ace37c34be74ff
Adam <adam.kilis@yahoo.com>
2020-05-07 23:16:32 +0200

    period selectors feature

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -3,20 +3,28 @@ import {AreaChart, Area, XAxis, YAxis, Tooltip} from "recharts"
 
 const Chart: React.FC = () => {
     const currentTimestamp: number = Math.floor( new Date().getTime()/1000);
-    const BASE_CRYPTO: string = `USDT`;
-    const CRYPTO: string = `ETH`
-    // const days: number = 90;   //max 90
     const day: number = 24*60*60;
     const week: number = 7*day;
-    const weeks: number = 143;   //max 143
+    const month: number = 30*day;
+    let days: number = 30;   //max 90
+    let weeks: number = 54;   //max 143
+    let months: number = 48;
     const [xAxis, setXAxis] = useState<string[]>([]);
     const [yAxis, setYAxis] = useState<number[]>([]);
     const [dataSet, setDataSet] = useState<object[]>([]);
-
+    const [periodOption, setPeriodOption] = useState<string>(`1Y`);
+    const [baseCrypto, setBaseCrypto] = useState<string>('USDT');
+    const [crypto, setCrypto] = useState<string>('BTC')
+    const [timeFrame, setTimeFrame] = useState<string>('W')
+    const [timePeriod, setTimePeriod] = useState<number>(weeks*week);
+    const [baseTimeframe, setBaseTimeframe] = useState<number>(week);
+    
     function converteUnixToDate(arr: number[]) {
         const anotherArr: string[] = arr.map((item: number) => {
-            let options = {year: '2-digit', month: 'short', day: 'numeric'}
-            let d = new Date(item*1000).toLocaleDateString('us-US', options).replace(',', '');
+            let options1 = {year: '2-digit', month: 'short', day: 'numeric'};
+            let options2 = {year: '2-digit', month: 'short', day: 'numeric'}
+            let d = new Date(item*1000).toLocaleDateString('us-US', options1).replace(',', '');
+            let t = new Date(item*1000).toLocaleTimeString('us-US', options2)
             let m1: string = '';
             let d1: string = '';
             let y1: string = '';
@@ -25,48 +33,96 @@ const Chart: React.FC = () => {
         })
         setXAxis(anotherArr);
     }
-
-    const fetchItems = async () => {
+    
+    const fetchItems = async (baseCrypto: string, crypto: string, timeFrame: string, timePeriod: number) => {
         const API_KEY: string | number | undefined = process.env.REACT_APP_API_KEY;
-        let TIMEFRAMES: string = `W`;
-        let UNIX_FROM: string = `${currentTimestamp - (weeks*week)}`;
+        let UNIX_FROM: string = `${currentTimestamp - timePeriod}`;
         let UNIX_TO: number = currentTimestamp;
+        let BASE_CRYPTO: string = baseCrypto;
+        let CRYPTO: string = crypto;
+        let TIMEFRAMES: string = timeFrame;
         // let API_CALL_CURRENCY_LIST: string = `https://finnhub.io/api/v1/crypto/symbol?exchange=binance&token=${API_KEY}`
         let API_CALL_CRYPTO_VALUE: string = `https://finnhub.io/api/v1/crypto/candle?symbol=BINANCE:${CRYPTO}${BASE_CRYPTO}&resolution=${TIMEFRAMES}&from=${UNIX_FROM}&to=${UNIX_TO}&token=${API_KEY}`
         let response = await fetch(API_CALL_CRYPTO_VALUE);
         let data = await response.json();
         setYAxis(data.o); 
-        console.log(data.o)
+        console.log(data.o.length)
     }
-    function createXAxis() {
+    function createXAxis(baseTimeframe: number) {
         let newArr: number[] = [currentTimestamp];
         let value: number = currentTimestamp;
-        for(let i = 1; i < weeks; i++){
-            newArr.push(value - week);
-            value = value - week;
+        for(let i = 1; i < yAxis.length; i++){
+            newArr.push(value - baseTimeframe);
+            value = value - baseTimeframe;
         }
         newArr.reverse() 
         converteUnixToDate(newArr);
     }
     
     useEffect(() => {
-        fetchItems()
-        createXAxis()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[currentTimestamp])
-
-    useEffect(() => {
-        const data = [];
-        for (let i = 0; i< xAxis.length; i++){
-            data.push({Date: xAxis[i], Value: yAxis[i]})
+        switch(periodOption){
+            case "1H":
+                console.log('1H')
+                break;
+            case "1D":
+                console.log('1D')
+                break;
+            case "1W":
+                console.log('1W')
+                setTimeFrame('D');
+                setTimePeriod(day*days);
+                setBaseTimeframe(day);
+                break;
+            case "1M":
+                console.log('1M')
+                setTimeFrame('D');
+                setTimePeriod(day*days);
+                setBaseTimeframe(day);
+                break;
+            case "1Y":
+                console.log('1Y')
+                setTimeFrame('W');
+                setTimePeriod(week*weeks);
+                setBaseTimeframe(week);
+                break;
+            case "ALL":
+                console.log('ALL')
+                setTimeFrame('M');
+                setTimePeriod(month*months);
+                setBaseTimeframe(month);
+                break;
+            default:
+                setTimeFrame('W');
+                setTimePeriod(week*weeks);
+                setBaseTimeframe(week);
         }
-        setDataSet(data)
+        fetchItems(baseCrypto, crypto, timeFrame, timePeriod)
+        console.log('action')
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[periodOption])
+    
+    useEffect(() => {
+        if ((yAxis.length) > 0) {
+            console.log(baseTimeframe, week, "second")
+            createXAxis(baseTimeframe)
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [yAxis])
     
+    useEffect(() => {
+        if ((xAxis.length) > 0) {
+            const data: object[] = [];
+            for (let i = 0; i < yAxis.length; i++){
+                data.push({Date: xAxis[i], Value: yAxis[i]})
+            }
+            setDataSet(data)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [xAxis])
+
     return (
         <div className="chart">
-            <div className="chart__title">{CRYPTO} / {BASE_CRYPTO}</div>
+            <div className="chart__title">ETH / USDT</div>
             <AreaChart
                 width={1000}
                 height={400}
@@ -74,7 +130,6 @@ const Chart: React.FC = () => {
                 margin={{
                 top: 5, right: 30, left: 20, bottom: 5,
                 }}
-            
             >
                 <defs>
                     <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
@@ -83,7 +138,7 @@ const Chart: React.FC = () => {
                     </linearGradient>
                 </defs>
                 <XAxis dataKey="Date" />
-                <YAxis unit={BASE_CRYPTO === `USDT` ? ` $` : ``}/>
+                <YAxis unit={` $`}/>
                 <Tooltip />
                 <Area
                     type="monotone" 
@@ -94,6 +149,26 @@ const Chart: React.FC = () => {
                     fill='url(#color)'
                 />
             </AreaChart>
+            <div className="period-selector">
+            <div className={`period-selector__option ${periodOption === "1H" ? 'period-selector__option--active' : ''}`} onClick={() => setPeriodOption("1H")}>
+                    <span>1H</span>
+                </div>
+                <div className={`period-selector__option ${periodOption === "1D" ? 'period-selector__option--active' : ''}`} onClick={() => setPeriodOption("1D")}>
+                    <span>1D</span>
+                </div>
+                <div className={`period-selector__option ${periodOption === "1w" ? 'period-selector__option--active' : ''}`} onClick={() => setPeriodOption("1W")}>
+                    <span>1W</span>
+                </div>
+                <div className={`period-selector__option ${periodOption === "1M" ? 'period-selector__option--active' : ''}`} onClick={() => setPeriodOption("1M")}>
+                    <span>1M</span>
+                </div>
+                <div className={`period-selector__option ${periodOption === "1Y" ? 'period-selector__option--active' : ''}`} onClick={() => setPeriodOption("1Y")}>
+                    <span>1Y</span>
+                </div>
+                <div className={`period-selector__option ${periodOption === "ALL" ? 'period-selector__option--active' : ''}`} onClick={() => setPeriodOption("ALL")}>
+                    <span>ALL</span>
+                </div>
+            </div>
         </div>
     )
 }
